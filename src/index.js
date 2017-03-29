da35326beb2c856ef5b1b34f10b3fcde1f2d9871
Capi Etheriel <barraponto@gmail.com>
2017-03-29 00:04:59 -0300

    fix(config): default middleware options

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const postcss = require('neutrino-middleware-postcss');
 const cssnext = require('postcss-cssnext');
 const merge = require('deepmerge');
 
-module.exports = (neutrino, options) => {
+module.exports = (neutrino, options = {}) => {
   const defaultOptions = {
     plugins: [
       cssnext()
