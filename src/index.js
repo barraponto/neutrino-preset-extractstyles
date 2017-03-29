c9d5555edfdac78b5830d1ccf04cd738f0285527
Capi Etheriel <barraponto@gmail.com>
2017-03-28 21:53:14 -0300

    fix(require): fixes postcss-cssnext require

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const postcss = require('neutrino-middleware-postcss');
 
 module.exports = (neutrino) => {
-  const cssnext = require.resolve('postcss-cssnext');
+  const cssnext = require('postcss-cssnext');
   neutrino.use(postcss, {
     plugins: [
       cssnext()
